5a01d0288769da2efc08306af678a1f46c1c4167
Bryan <Uriasbe@gmail.com>
2020-07-27 23:34:26 -0500

    Made crypto changes instead of stock

--- scripts/chart.js
+++ scripts/chart.js
@@ -1,48 +1,47 @@
 
-//this will obtain the cryptocurrency api for the graph (BitCoin)
-// let bitcoinURL = "https://api.coinlore.net/api/ticker/?id=90"
-
-
-// fetch(bitcoinURL)
-// .then(function(response) {
-//     return response.json();
-     
-// })
-// .then (function(data) {
-//     console.log(data);
-// });
-
-
-
-
-//Too many tries in one day. Will need to wait
-
-// let bitcoinURL = `https://cors-anywhere.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest`
-// let qString = "?CMC_PRO_API_KEY=27e7f55d-4a48-4cc4-b254-008fcfbeae60"
-
-// fetch(bitcoinURL+qString)
-// .then(response => response.json())
-// .then(data => {
-//     console.log(data)
-
-// })
 
 // This will be where my data goes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 async function createChart() {
 
     let ibmDataClose = []
     let dailyData = {
         open: 0,
-        close: 0
+        open1: 0,
+        open2: 0,
+        open3: 0,
+        open4: 0,
+        close: 0,
+        day1: 0,
+        day2: 0,
+        day3: 0,
+        day4: 0
     }
     
     
     let apikey = "4WNOTMZ6XCS94HP0"
     
     async function getData() {
-        const response = await fetch("https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey="+apikey);
+        const response = await fetch("https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=BTC&market=USD&apikey="+apikey);
         const data = await response.json();
+        console.log(data)
         
         //ibmDataOpen.push((data["Weekly Time Series"]["2020-07-27"][["1. open"]]))
         // ibmDataClose.push((data["Weekly Time Series"]["2020-07-27"][["4. close"]]))
@@ -51,126 +50,159 @@ async function createChart() {
         // console.log(data["Weekly Time Series"]["2020-07-27"][["1. open"]])
     
        
-    
-        // let dailyHigh = 
-        // let dailyLow = 
+   
         
         //this lets me know about the object length
-        let size = Object.keys(data["Weekly Time Series"])
-        console.log(data["Weekly Time Series"])
-        console.log(data["Weekly Time Series"][size[0]])
-        console.log(data["Weekly Time Series"][size[0]]["1. open"])
+        let size = Object.keys(data["Time Series (Digital Currency Daily)"])
+        // console.log(data["Weekly Time Series"])
+        // console.log(data["Weekly Time Series"][size[0]])
+        // console.log(data["Weekly Time Series"][size[0]]["1. open"])
     
         // console.log(size[0][1])
         // console.log(size[1])
         
      
-        dailyData.open = (data["Weekly Time Series"][size[0]]["1. open"]);
-        dailyData.close = (data["Weekly Time Series"][size[0]]["4. close"])
-        console.log(`daily data updated in get data function: ${dailyData.open}`)
+        //Market Open for Bitcoin
+        dailyData.open = (data["Time Series (Digital Currency Daily)"][size[0]]["1a. open (USD)"]);
+        dailyData.open1 = (data["Time Series (Digital Currency Daily)"][size[1]]["1a. open (USD)"]);
+        dailyData.open2 = (data["Time Series (Digital Currency Daily)"][size[2]]["1a. open (USD)"]);
+        dailyData.open3 = (data["Time Series (Digital Currency Daily)"][size[3]]["1a. open (USD)"]);
+        dailyData.open4 = (data["Time Series (Digital Currency Daily)"][size[4]]["1a. open (USD)"]);
         
-    
         
+        
+        
+        
+        
+        dailyData.close = (data["Time Series (Digital Currency Daily)"][size[0]]["4. close"])
+        // console.log(`daily data updated in get data function: ${dailyData.open}`)
+
+        console.log(size)
+        
+        dailyData.day = size[0]
+        dailyData.day1 = size[1]
+        dailyData.day2 = size[2]
+        dailyData.day3= size[3]
+        dailyData.day4 = size[4]
        
+        
+      
     }
     
     await getData()
     
     
     
-    console.log(`This is Daily Data.Open ${dailyData.open}`)
+    // console.log(`This is Daily Data.Open ${dailyData.open}`)
     
     
-    let crytoChart = new Chart(myChart, {
-        type: 'bar',
+    // let crytoChart = new Chart(myChart, {
+    //     type: 'line',
+    //     data: {
+    //         labels: ['BitCoin'],
+    //         datasets: [{
+    //             label: 'CyptoCurrency Price Per Day',
+    //             data: [dailyData.open, dailyData.close],
+    //             backgroundColor: [
+    //                 'rgba(255, 99, 132, 0.2)',
+    //                 'rgba(54, 162, 235, 0.2)',
+    //                 'rgba(255, 206, 86, 0.2)',
+    //                 'rgba(75, 192, 192, 0.2)',
+    //                 'rgba(153, 102, 255, 0.2)',
+    //                 'rgba(255, 159, 64, 0.2)'
+    //             ],
+    //             borderColor: [
+    //                 'rgba(255, 99, 132, 1)',
+    //                 'rgba(54, 162, 235, 1)',
+    //                 'rgba(255, 206, 86, 1)',
+    //                 'rgba(75, 192, 192, 1)',
+    //                 'rgba(153, 102, 255, 1)',
+    //                 'rgba(255, 159, 64, 1)'
+    //             ],
+    //             borderWidth: 1,
+    //             borderColor:"#777",
+    //             hoverBorderWidth: 3,
+    //             hoverBorderColor: "#000"
+    //         }]
+    //     },
+    //     options: {
+    //         scales: {
+    //             yAxes: [{
+    //                 ticks: {
+    //                     beginAtZero: false
+    //                 }
+    //             }]
+    //         }
+    //     }
+    // });
+    
+
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+
+        // The data for our dataset
         data: {
-            labels: ['BitCoin', 'Etherum', 'Lite'],
+            labels: [dailyData.day4,dailyData.day3,dailyData.day2,dailyData.day1,dailyData.day],
             datasets: [{
-                label: 'CyptoCurrency Price Per Day',
-                data: [dailyData.open, dailyData.close],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1,
-                borderColor:"#777",
-                hoverBorderWidth: 3,
-                hoverBorderColor: "#000"
+                label: "BitCoin Trend",
+                backgroundColor: 'rgb(99, 99, 132)',
+                borderColor: 'rgb(99, 99, 132)',
+                data: [dailyData.open4,dailyData.open3, dailyData.open2, dailyData.open1, dailyData.open],
             }]
         },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: false
-                    }
-                }]
-            }
-        }
+
+        // Configuration options go here
+        options: {}
     });
+
+
     
-    
-    console.log(ibmDataOpen)
+   
 
 }
 
 createChart()
 
-// let ibmDataClose = []
-// let dailyData = {
-//     open: 234,
-//     close: 446
-// }
 
 
-// let apikey = "4WNOTMZ6XCS94HP0"
 
-// async function getData() {
-//     const response = await fetch("https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey="+apikey);
-//     const data = await response.json();
-    
-//     //ibmDataOpen.push((data["Weekly Time Series"]["2020-07-27"][["1. open"]]))
-//     // ibmDataClose.push((data["Weekly Time Series"]["2020-07-27"][["4. close"]]))
-//     // console.log(data["Meta Data"])
-//     // console.log(data["Weekly Time Series"]["2020-07-27"])
-//     // console.log(data["Weekly Time Series"]["2020-07-27"][["1. open"]])
 
-   
 
-//     // let dailyHigh = 
-//     // let dailyLow = 
-    
-//     //this lets me know about the object length
-//     let size = Object.keys(data["Weekly Time Series"])
-//     console.log(data["Weekly Time Series"])
-//     console.log(data["Weekly Time Series"][size[0]])
-//     console.log(data["Weekly Time Series"][size[0]]["1. open"])
 
-//     // console.log(size[0][1])
-//     // console.log(size[1])
-    
- 
-//     dailyData.open = 600;
-//     console.log(`daily data updated in get data function: ${dailyData.open}`)
-    
 
-    
-   
-// }
 
-// getData()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -218,4 +250,3 @@ createChart()
 // });
 
 
-// console.log(ibmDataOpen)
