de61047d69618624e6e87d42b16b06c345c21ef8
Jeremy <jeremy.stegall77@gmail.com>
2020-08-01 13:14:22 -0400

    added updated dashboard

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,48 +1,49 @@
 
 
-// fetch("https://finnhub-realtime-stock-price.p.rapidapi.com/stock/symbol?exchange=US", {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-host": "finnhub-realtime-stock-price.p.rapidapi.com",
-// 		"x-rapidapi-key": "d9a11c1fb1mshd696b88c6fec22dp13a29djsn713c2dd0576a"
-// 	}
-// })
-// .then(function(response){
-// 	return response.json();
-// }).then(function(data){
-// 	for(let i = 0; i < data.value.length; i++){
-// 		let container = document.createElement("div");
-// 		let cardBody = document.createElement("div");
-// 		let name = document.createElement("h5");
-// 		let image = document.createElement("img");
-// 		let description = document.createElement("p");
-// 		let mainContainer = document.getElementById("mainContent");
+ fetch("https://bing-news-search1.p.rapidapi.com/news?safeSearch=Off&textFormat=Raw", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "bing-news-search1.p.rapidapi.com",
+		"x-rapidapi-key": "947d668e14msh8a1f9ad2d4973b9p1a6cc9jsne6045d441cfa",
+		"x-bingapis-sdk": "true"
+	}
+ })
+ .then(function(response){
+ 	return response.json();
+ }).then(function(data){
+ 	for(let i = 0; i < data.value.length; i++){
+ 		let container = document.createElement("div");
+ 		let cardBody = document.createElement("div");
+ 		let name = document.createElement("h5");
+ 		let image = document.createElement("img");
+ 		let description = document.createElement("p");
+		let mainContainer = document.getElementById("mainContent");
 		
-// 		container.classList.add("card");
-// 		cardBody.classList.add("card-body");
-// 		name.classList.add("card-title");
-// 		image.classList.add("card-image-top");
-// 		description.classList.add("card-text");
+ 		container.classList.add("card");
+ 		cardBody.classList.add("card-body");
+ 		name.classList.add("card-title");
+ 		image.classList.add("card-image-top");
+ 		description.classList.add("card-text");
 
 
 		
-// 		name.innerHTML = data.value[i].name;
-// 		description.innerHTML = data.value[i].description;
-// 		image.src = data.value[i].image.thumbnail.contentUrl;
+ 		name.innerHTML = data.value[i].name;
+ 		description.innerHTML = data.value[i].description;
+ 		image.src = data.value[i].image.thumbnail.contentUrl;
 
-// 		cardBody.appendChild(image);
-// 		cardBody.appendChild(name);
-// 		cardBody.appendChild(description);
+ 		cardBody.appendChild(image);
+ 		cardBody.appendChild(name);
+		cardBody.appendChild(description);
 		
-// 		container.appendChild(cardBody);
-// 		mainContainer.appendChild(container)
+ 		container.appendChild(cardBody);
+ 		mainContainer.appendChild(container)
 		
-// 	}
-// 	console.log(data);
-// 	/*document.getElementById('news').innerHTML=data.value[0].description;*/
-// }).catch(function(error){
-// 	console.log(error);
-// })
+ 	}
+ 	console.log(data);
+ 	/*document.getElementById('news').innerHTML=data.value[0].description;*/
+ }).catch(function(error){
+ 	console.log(error);
+})
 
 
 
@@ -52,7 +53,7 @@
 
 
 
-let apiKey = "e838ab174e5a8f76c25a6becfa21fd618bd73751"
+/*let apiKey = "e838ab174e5a8f76c25a6becfa21fd618bd73751"
 
   async function createNewsChart() {
 
@@ -103,4 +104,4 @@ let apiKey = "e838ab174e5a8f76c25a6becfa21fd618bd73751"
  loadTicker.addEventListener("click", function () {
      console.log("You clicked me")
      createTickChart()
- })
+ })*/
